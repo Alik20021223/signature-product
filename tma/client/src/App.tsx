03ce0f658f774ba795c2ac9295434e3cbffe2b7d
Alisher <alik20021223@gmail.com>
2025-01-18 16:15:25 +0500

    doing architacture

---
+++ tma/client/src/App.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import './App.css'
+const tg = window.Telegram.WebApp
+function App() {
+
+  useEffect(() => {
+    tg.ready()
+  }, [])
+  
+  function OnClose() {
+    tg.close()
+  }
+
+  return (
+    <>
+      <div className='App'>
+        <button onClick={OnClose}>Закрыть</button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tma/server/index.js
@@ -0,0 +1,42 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+// Замените значение ниже на токен, который вы получили от @BotFather
+const token = '7750952019:AAH5VhoGT7Y-9FaNzi_7NMGIqUy2Hn13srE';
+
+// Создайте бота, который использует 'polling' для получения новых обновлений
+const bot = new TelegramBot(token, { polling: true });
+
+// Настройки кнопки для запуска приложения
+const startOptions = {
+    reply_markup: {
+        inline_keyboard: [
+            [{ text: 'Нажмите для запуска приложения', web_app: {url: 'https://ya.ru/'} }]
+        ]
+    }
+};
+
+// Устанавливаем команды для бота
+bot.setMyCommands([
+    { command: 'start', description: 'Начальное приветствие' },
+    { command: 'start_app', description: 'Запуск приложения' },
+]);
+
+
+// Обработка сообщений
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    switch (text) {
+        case '/start':
+            bot.sendMessage(chatId, 'Добро пожаловать в тестовое приложение Alishers-tma');
+            break;
+
+        case '/start_app':
+            bot.sendMessage(chatId, 'Нажмите кнопку для запуска', startOptions);
+            break;
+
+        default:
+            bot.sendMessage(chatId, 'Команда не распознана. Попробуйте еще раз.');
+    }
+});
